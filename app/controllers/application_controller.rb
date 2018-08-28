89d2adf0f15360b7005750d5b49cbd346cd83621
sho.endo <s.endo.8951@gmail.com>
2018-08-28 05:38:31 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  include SessionsHelper
+  
+  private
+  def required_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
